fcb5bf01e198f03c737f6bd75d96c305fc125f38
jinseongwon99 <jinks0315@naver.com>
2024-10-17 13:16:17 +0900

    first commit

---
+++ src/views/HelloVue.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello Vue js</h1>
+    <h1>{{msg}}</h1>
+    <p :title="msg">타이틀이 보입니다</p> 
+    <input type="text" v-model="seen">
+    <p v-if = "seen == 'test' ">if가 보입니다</p> 
+    <p v-else-if = "seen == 'test1'">else if가 보입니다</p> 
+    <p v-else>else가 보입니다</p> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloVue',
+  data() {
+    return {
+      msg: "vue Hello js"
+      ,seen : 'test' 
+    };
+  }
+}
+</script>
